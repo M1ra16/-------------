d1d51450e836493e4dbb86af753974772247fd0b
Sidorova Maria <maria-masha98@mail.ru>
2021-11-30 20:59:24 +0300

    first commit

---
+++ js/main1.js
@@ -0,0 +1,24 @@
+let slider=document.querySelector('.slider')
+let count=0;
+
+setInterval(function autop(){
+    count +=500
+    if (count>3500){
+        count=0
+    }
+    slider.style.left=-count+'px';
+}, 3500)
+
+
+$(function() {
+    $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+    $('#topNubex').fadeIn();
+    } else {
+    $('#topNubex').fadeOut();
+    }
+    });
+    $('#topNubex').click(function() {
+    $('body,html').animate({scrollTop:0},700);
+    });
+});
---
+++ js/main5.js
@@ -0,0 +1,11 @@
+let mail=document.querySelector('.name-js')
+let phone=document.querySelector('.phone-js')
+
+function regi(){
+    if (mail !=''){
+        alert("Добро пожаловать!")
+        window.location.href="file:///C:/Users/семья/Desktop/курсовая%20сайт/index.html"
+    }
+    else {
+        alert("Незваный гость. Уходи!")}
+}
