c428725458f7aa148304045891873ff5ec01e038
Sidorova Maria <maria-masha98@mail.ru>
2021-12-01 18:16:03 +0300

    -

--- js/main5.js
+++ js/main5.js
@@ -1,10 +1,11 @@
 let mail=document.querySelector('.name-js')
 let phone=document.querySelector('.phone-js')
+let elem1 = document.querySelector('.btn-js')
 
 function regi(){
-    if (mail !=''){
-        alert("Добро пожаловать!")
-        window.location.href="file:///C:/Users/семья/Desktop/курсовая%20сайт/index.html"
+    if (mail.value !='' && phone.value !=''){
+        alert("Спасибо за то, что выбрали нас. В ближайшее время мы с вами свяжемся.")
+        window.location.href="index.html"
     }
     else {
         alert("Незваный гость. Уходи!")}
